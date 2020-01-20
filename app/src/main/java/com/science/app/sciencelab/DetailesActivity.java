10b78a544262f989322e3ea03d461e656e1daab6
Hamzashami <shamihamza25@gmail.com>
2020-01-20 23:16:37 +0200

    add

---
+++ app/src/main/java/com/science/app/sciencelab/DetailesActivity.java
@@ -0,0 +1,54 @@
+package com.science.app.sciencelab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class DetailesActivity extends AppCompatActivity {
+    private ImageView iv_item;
+    private TextView tv_name, tv_details;
+    Item item;
+    Intent intent;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailes);
+        initViews();
+        initItems();
+        initClicks();
+
+    }
+
+    private void initClicks() {
+
+    }
+
+    private void initItems() {
+        intent = getIntent();
+        int id = intent.getIntExtra("id", 0);
+
+        if (id != 0) {
+            item = new MyDbHelper(DetailesActivity.this).getItemById(id);
+            iv_item.setImageResource(item.getImageId());
+            tv_name.setText(item.getName());
+            tv_details.setText(item.getDetails());
+
+        }
+
+    }
+    private void initViews() {
+        iv_item = findViewById(R.id.iv_item);
+        tv_name = findViewById(R.id.tv_title);
+        tv_details = findViewById(R.id.tv_details);
+        toolbar =findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
