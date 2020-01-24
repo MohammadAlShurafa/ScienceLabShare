8ef521834ead60d666cae6fd6ff9d8c6c6bb77d1
MohammadAlShurafa <43353717+MohammadAlShurafa@users.noreply.github.com>
2020-01-24 22:33:43 +0200

    details

--- app/src/main/java/com/science/app/sciencelab/DetailsActivity.java
+++ app/src/main/java/com/science/app/sciencelab/DetailsActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class DetailesActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
     private ImageView iv_item;
     private TextView tv_name, tv_details;
     Item item;
@@ -27,7 +27,7 @@ public class DetailesActivity extends AppCompatActivity {
     }
 
     private void initClicks() {
-        
+
     }
 
     private void initItems() {
@@ -35,7 +35,7 @@ public class DetailesActivity extends AppCompatActivity {
         int id = intent.getIntExtra("id", 0);
 
         if (id != 0) {
-            item = new MyDbHelper(DetailesActivity.this).getItemById(id);
+            item = new MyDbHelper(DetailsActivity.this).getItemById(id);
             iv_item.setImageResource(item.getImageId());
             tv_name.setText(item.getName());
             tv_details.setText(item.getDetails());
